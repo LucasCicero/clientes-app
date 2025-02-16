bb229fec7fe3e545f5686a9e0bc86548e7f1d9df
lucas <lucas.cicero94@gmail.com>
2025-02-15 21:29:35 -0300

    feature: Modulo lista de serviços

--- src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from 'src/app/clientes/cliente';
 import { ClientesService } from 'src/app/clientes.service';
+import { ServicoPrestado } from '../servicoPrestado';
+import { ServicoPrestadoService } from 'src/app/servico-prestado.service';
 
 @Component({
   selector: 'app-servico-prestado-form',
@@ -10,10 +12,16 @@ import { ClientesService } from 'src/app/clientes.service';
 export class ServicoPrestadoFormComponent implements OnInit {
 
   clientes: Cliente[] =[]
+  servico: ServicoPrestado;
+  success:boolean= false;
+  errors:String[];
 
   constructor(
-    private clienteService: ClientesService
-  ) { }
+    private clienteService: ClientesService,
+    private service : ServicoPrestadoService
+  ) {
+    this.servico = new ServicoPrestado();
+   }
 
   ngOnInit(): void {
     this.clienteService
@@ -22,8 +30,16 @@ export class ServicoPrestadoFormComponent implements OnInit {
   }
 
   OnSubmit(){
-    console.log('submit')
-     
+    this.service
+      .salvar(this.servico)
+      .subscribe(response =>{
+        this.success = true;
+        this.errors= null;
+        this.servico = new ServicoPrestado();  
+     }, errorResponse=>{
+      this.success=false;
+      this.errors = errorResponse.error.errors;
+    })
   }
 
 }
