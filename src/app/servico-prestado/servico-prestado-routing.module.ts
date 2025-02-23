3fd2526caaae229979dc5361be7584d03be04f5d
lucas <lucas.cicero94@gmail.com>
2025-02-22 22:55:50 -0300

    feature: Atualização das rotas

--- src/app/servico-prestado/servico-prestado-routing.module.ts
+++ src/app/servico-prestado/servico-prestado-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ServicoPrestadoFormComponent } from './servico-prestado-form/servico-prestado-form.component';
+import { LayoutComponent } from '../layout/layout.component';
 
 
 const routes: Routes = [
-  {path: 'servico-prestado-form',component:ServicoPrestadoFormComponent},
-  {path: 'servico-prestado-listagem', component: ServicoPrestadoFormComponent}
+  {path: 'servicos-prestados', component:LayoutComponent, children:[
+    {path: 'form',component:ServicoPrestadoFormComponent},
+    {path: 'lista', component: ServicoPrestadoFormComponent},
+    {path: '', redirectTo:'/servico-prestados/lista', pathMatch:'full'}
+  ]},
 ];
 
 @NgModule({
